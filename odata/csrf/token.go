becbc9ba4fe2c4c5328bdc2d4e31fba8d0ebff2f
Phani Puttabakula <phani.p@bluefunda.com>
2023-05-18 18:13:45 -0700

    csrf new token logic

--- odata/csrf/token.go
+++ odata/csrf/token.go
@@ -47,3 +47,40 @@ func (s *Service) Get() (*string, error) {
 	t := resp.Header.Get("X-Csrf-Token")
 	return &t, nil
 }
+
+type Srv struct {
+	client *httpclient.Clnt
+}
+
+func (s *Srv) GetToken(ctx context.Context) (*string, error) {
+
+	resp, err := s.client.Get(ctx, "")
+	if err != nil {
+		return nil, err
+	}
+	t := resp.Header.Get("X-Csrf-Token")
+
+	return &t, nil
+}
+
+func NewTk(host, auth string) (*string, error) {
+
+	c := httpclient.NewClnt(
+		http.DefaultClient,
+		httpclient.Options{
+			HostUrl:   host,
+			RootPath:  os.Getenv("UMC_SERVICE"),
+			SapClient: os.Getenv("SAP_CLIENT"),
+			AuthToken: auth,
+			Verbose:   false,
+		})
+	s := new(Srv)
+	s.client = c
+	ctx := context.TODO()
+	tk, err := s.GetToken(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return tk, nil
+
+}
--- odata/csrf/token_test.go
+++ odata/csrf/token_test.go
@@ -29,3 +29,24 @@ func TestGet(t *testing.T) {
 		t.Errorf("expected %s, got %s", exp, *got)
 	}
 }
+
+func TestNewTk(t *testing.T) {
+
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("X-Csrf-Token", "token")
+		w.Header().Add("Authorization", "auth")
+		w.Header().Add("sap-client", "100")
+	}))
+	defer svr.Close()
+
+	exp := "token"
+	got, err := csrf.NewTk(svr.URL, "auth")
+	if err != nil {
+		t.Error("error here")
+		t.Errorf(err.Error())
+	}
+
+	if *got != exp {
+		t.Errorf("expected %s, got %s", exp, *got)
+	}
+}
