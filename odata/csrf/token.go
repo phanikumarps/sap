f7017040a8d48732745ce60cbfb39afe1c511a2b
Phani Puttabakula <phani.p@bluefunda.com>
2023-05-18 23:10:36 -0700

    total changes

--- odata/csrf/token.go
+++ odata/csrf/token.go
@@ -5,52 +5,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/phanikumarps/sap/odata/config"
 	"github.com/phanikumarps/sap/odata/httpclient"
 )
-
-func NewToken(ctx context.Context, host, clnt, auth string) (*string, error) {
-	s := NewService(
-		host,
-		clnt,
-		auth,
-	)
-	t, err := s.Get()
-	if err != nil {
-		return nil, err
-	}
-	return t, nil
-}
-
-type Service struct {
-	httpclient.Client
-}
-
-func NewService(host, sapClient, authToken string) *Service {
-	s := new(Service)
-	s.Client = *httpclient.New(
-		httpclient.WithHost(host),
-		httpclient.WithSapClient(sapClient),
-		httpclient.WithAuthToken(authToken),
-	)
-	return s
-}
-
-func (s *Service) Get() (*string, error) {
-	ctx := context.TODO()
-	resource := httpclient.RequestOptions{Path: ""}
-	r := config.DefaultRootPath(os.Getenv("UMC_SERVICE"))
-	resp, err := s.Call(ctx, http.MethodHead, string(*r), resource.Path, nil, "")
-	if err != nil {
-		return nil, err
-	}
-	t := resp.Header.Get("X-Csrf-Token")
-	return &t, nil
-}
-
-type Srv struct {
-	client *httpclient.Clnt
-}
 
 func (s *Srv) GetToken(ctx context.Context) (*string, error) {
 
@@ -63,9 +19,13 @@ func (s *Srv) GetToken(ctx context.Context) (*string, error) {
 	return &t, nil
 }
 
-func NewTk(host, auth string) (*string, error) {
+type Srv struct {
+	client *httpclient.Clnt
+}
 
-	c := httpclient.NewClnt(
+func NewSrv(host, auth string) *Srv {
+	s := new(Srv)
+	s.client = httpclient.NewClnt(
 		http.DefaultClient,
 		httpclient.Options{
 			HostUrl:   host,
@@ -74,8 +34,12 @@ func NewTk(host, auth string) (*string, error) {
 			AuthToken: auth,
 			Verbose:   false,
 		})
-	s := new(Srv)
-	s.client = c
+	return s
+}
+
+func NewTk(host, auth string) (*string, error) {
+
+	s := NewSrv(host, auth)
 	ctx := context.TODO()
 	tk, err := s.GetToken(ctx)
 	if err != nil {
--- odata/csrf/token_test.go
+++ odata/csrf/token_test.go
@@ -8,28 +8,6 @@ import (
 	"github.com/phanikumarps/sap/odata/csrf"
 )
 
-func TestGet(t *testing.T) {
-
-	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("X-Csrf-Token", "token")
-		w.Header().Add("Authorization", "auth")
-		w.Header().Add("sap-client", "100")
-	}))
-	defer svr.Close()
-
-	exp := "token"
-	s := csrf.NewService(svr.URL, "100", "auth")
-	got, err := s.Get()
-	if err != nil {
-		t.Error("error here")
-		t.Errorf(err.Error())
-	}
-
-	if *got != exp {
-		t.Errorf("expected %s, got %s", exp, *got)
-	}
-}
-
 func TestNewTk(t *testing.T) {
 
 	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
