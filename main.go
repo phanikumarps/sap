ce029659774d717072694f6ed39f0bf367747c74
Phani Puttabakula <phani.p@bluefunda.com>
2023-05-19 17:56:52 -0700

    main file

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/phanikumarps/sap/odata"
+	"github.com/phanikumarps/sap/odata/isu/umc"
+)
+
+func main() {
+	d := odata.SetDefaults()
+	s := umc.NewService(d.HostUrl, d.AuthToken)
+	ctx := context.TODO()
+	acct, err := s.GetAccount(ctx, "1234")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(acct)
+}
